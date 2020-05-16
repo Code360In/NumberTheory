a1985d0fffbd5d35eea854984b994fe9d205bf9b
Trinh Cao Van Duc <44729657+PanAngelosk@users.noreply.github.com>
2020-05-16 13:33:14 +0700

    BT1 - Done!

--- BT_Chuong_1/Bai_1/1_1712362.py
+++ BT_Chuong_1/Bai_1/1_1712362.py
@@ -1,48 +1,81 @@
-# from sys import argv
-# script, input = argv
+from sys import argv
+script, input = argv
 
 
 log = print
 
-
-# ham chuyen co so
-
-
-# def convert_number_nab(n, a, b):
-#     if(b == 10):
-
-
-# log(oct(1001))
-# log(bin(1001))
 # log(int("1001", base=2))
-# log(int("FAB", base=16))
-
-# log(int("123", base=4, base=5))
-# int()
-
-# ham chuyen he 2 sang he 10
+# func chuyen co so n sang 10
 
 
-def convert_base_nab(n, a, b):
-    log("n = ", n)
-    log("a = ", a)
-    log("b = ", b)
-    convert = int("%s" % (n), base=a)
-    log("Result = ", convert)
+def convert_basen_to_base10(value, base_a):
+    str_val = str(value)
+    base_10 = int(str_val, base=base_a)
+    return base_10
 
 
-# convert_base_nab(123, 4, 5)
+base_10 = convert_basen_to_base10(123, 4)
+# log(base_10)
+
+
+# func chuyen co so 10 sang b
+def convert_base10_to_baseb(base_10, base_b):
+    if (base_10 < 0 or base_b < 2 or base_b > 16):
+        return ""
+    sb = ""
+    m = 0
+    remainder = base_10
+
+    while (remainder > 0):
+        if (base_b > 10):
+            m = remainder % base_b
+            if (m >= 10):
+                sb = sb + str(chr(55 + m))
+            else:
+                sb = sb + str(m)
+        else:
+            sb = sb + str(remainder % base_b)
+        remainder = int(remainder / base_b)
+    return "".join(reversed(sb))
+
+
+# log(convert_number(123,5))
+# base_b = convert_base10_to_baseb(base_10, 5)
+# log(base_b)
+
+
+def divide_data(n, base_a, base_b):
+    base_10 = 0
+    base_k = 0
+    result = 0
+    if(base_b == 10):
+        base_10 = convert_basen_to_base10(n, base_a)
+        result += base_10
+    else:
+        base_10 = convert_basen_to_base10(n, base_a)
+        # log(base_10)
+        base_k = convert_base10_to_baseb(base_10, base_b)
+        # log(base_k)
+        result += int(base_k)
+    return result
+
+
+#data = divide_data(1234, 5, 6)
+# log(data)
 
 # doc file
 fr = open('input.txt', 'r')
 lines = [line[:-1].split() for line in fr.readlines()]
 fr.close()
-
-# log(lines)
+log(lines)
 
 
 # ghi file
-# fw = open('1_1712362.txt', 'wb')
-# for line in lines:
-#     log(line)
-#     # fw.write(line)
+fw = open('1_1712362.txt', 'w')
+for l in lines:
+    log(l)
+    result = divide_data(str(l[0]), int(l[1]), int(l[2]))
+    fw.write(str(result)+"\n")
+
+# fw.write(lines)
+fw.close()
